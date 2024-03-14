8f84dd1173161338973e449df477c1fc97496c53
lauravisentin <laura.vise@icloud.com>
2024-03-14 11:46:15 -0400

    Initial commit

---
+++ Assets/Scripts/DeplacementsMegaman.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementsMegaman : MonoBehaviour
+{
+    float vitesseX;      //vitesse horizontale actuelle
+    public float vitesseXMax;   //vitesse horizontale Maximale désirée
+    float vitesseY;      //vitesse verticale 
+    public float vitesseSaut;   //vitesse de saut désirée
+
+    public bool megamanCollision;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // déplacement vers la gauche
+        if (Input.GetKey("left"))
+        {
+            vitesseX = -vitesseXMax;
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+        else if (Input.GetKey("right"))   //déplacement vers la droite
+        {
+            vitesseX = vitesseXMax;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            vitesseX = GetComponent<Rigidbody2D>().velocity.x;  //mémorise vitesse actuelle en X
+        }
+
+        // sauter l'objet à l'aide la touche "w"
+        if (Input.GetKeyDown("up"))
+        {
+            vitesseY = vitesseSaut;
+            GetComponent<Animator>().SetBool("saut", true);
+            megamanCollision = false;
+        }
+        else
+        {
+            vitesseY = GetComponent<Rigidbody2D>().velocity.y;  //vitesse actuelle verticale
+        }
+
+
+        //Applique les vitesses en X et Y
+        GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
+
+
+        //**************************Gestion des animaitons de course et de repos********************************
+        //Active l'animation de course si la vitesse de déplacement n'est pas 0, sinon le repos sera jouer par Animator
+        if (vitesseX > 0.9f || vitesseX < -0.9f)
+        {
+            GetComponent<Animator>().SetBool("marche", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("marche", false);
+
+        }
+        if(vitesseX < 0.9f)
+        {
+            GetComponent<Animator>().SetBool("repos", true);
+        }
+
+        if (megamanCollision == true)
+        {
+            GetComponent<Animator>().SetBool("saut", false);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collisionsMegaman)
+    {
+        megamanCollision = true;
+    }
+}
