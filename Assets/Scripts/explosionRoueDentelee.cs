4abdadd2c1e03aa880243cf8b7e85e17fb110e92
lauravisentin <laura.vise@icloud.com>
2024-03-21 10:08:20 -0400

    FinPartie1

--- Assets/Scripts/explosionRoueDentelee.cs
+++ Assets/Scripts/explosionRoueDentelee.cs
@@ -15,4 +15,17 @@ public class explosionRoueDentelee : MonoBehaviour
     {
         
     }
+    void OnCollisionEnter2D(Collision2D infoCollision)
+    {
+        // Si le terrain est touché alors active l'animation de l'objet et détruit le
+        if (infoCollision.gameObject.name == "MegaMan")
+        {
+            GetComponent<Animator>().enabled = true;
+            GetComponent<Collider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            GetComponent<Rigidbody2D>().angularVelocity = 0;
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+            Destroy(gameObject, 0.5f);
+        }
+    }
 }
